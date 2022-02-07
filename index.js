d46ade17fb014db2417eae1e391476e07dbb08a9
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-07 11:28:57 -0300

    Add Procfile and prepare the port for deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 
 const server = http.createServer(app)
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
