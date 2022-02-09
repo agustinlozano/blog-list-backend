61f0674aced047e9d3c9228c474118a4744a982d
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-09 08:44:25 -0300

    Add and use: user controllers

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-const router = require('./controllers/blogs')
+const blogRouter = require('./controllers/blogs')
+const userRouter = require('./controllers/users')
 const {
   handleErrors,
   requestLogger,
@@ -24,7 +25,8 @@ app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
-app.use('/api/blogs', router)
+app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(notFound)
 app.use(handleErrors)
