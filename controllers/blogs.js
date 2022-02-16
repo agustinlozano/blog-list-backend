0ed7871eb1ab221e8ddb7d9bc4cdce411f2fa0de
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-16 19:08:14 -0300

    Make a relation between a user and a blog Now post blogs method link a user with a blog and vice versa.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,21 +1,50 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/Blogs')
+const User = require('../models/User')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {
+    username: 1,
+    name: 1
+  })
   response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-  const body = request.body
+  const {
+    title,
+    author,
+    url,
+    likes = 0,
+    user: userId
+  } = request.body
+
+  /* busco el usuario responsable de crear el blog */
+  const user = await User.findById(userId)
+
+  /* construyo el objeto del blog y no olvido atrubuir el usuario */
   const newBlog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0
+    title,
+    author,
+    url,
+    likes,
+    user: user._id
   })
 
+  /* guardo el nuevo blog en la base de datos */
   const savedBlog = await newBlog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+
+  const newUserInfo = {
+    username: user.username,
+    name: user.name,
+    blogs: user.blogs
+  }
+
+  /* Actualizo la informacion del usuario en la base de datos */
+  await User.findByIdAndUpdate(user._id, newUserInfo, { new: true })
+
   response.status(201).json(savedBlog)
 })
 
@@ -52,6 +81,7 @@ blogRouter.put('/:id', async (request, response) => {
   }
 
   const updatedBlog = await Blog.findByIdAndUpdate(id, updatedInfo, { new: true })
+
   if (updatedBlog) {
     response.json(updatedBlog)
   } else {
