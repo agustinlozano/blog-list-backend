b694e21aa75e0a2693cf257572775d1e851a5c87
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-09 19:03:12 -0300

    Add a new controller to reset the db when we are testing

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const logger = require('./utils/logger')
 const {
   handleErrors,
   requestLogger,
   notFound
 } = require('./utils/middleware')
+
+logger.info('connecting to', config.MONGODB_URI)
 
 /* conexion a mongoDB */
 mongoose.connect(config.MONGODB_URI)
@@ -29,6 +32,11 @@ app.use(requestLogger)
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing/', testingRouter)
+}
 
 app.use(notFound)
 app.use(handleErrors)
